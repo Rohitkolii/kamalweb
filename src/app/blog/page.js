48ee1fab45c9fa05b11ef93dde71248f21f9de60
Rohit koli <rohitkolisd@gmail.com>
2025-02-28 21:49:57 +0530

    blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,11 +1,13 @@
 import Navbar from '@/Components/Navbar/Navbar'
 import Blogs from '@/Components/Blogs/Blogs'
+import Footer from '@/Components/Footer/Footer'
 
 const page = () => {
   return (
     <div>
         <Navbar />
         <Blogs />
+        <Footer />
     </div>
   )
 }
