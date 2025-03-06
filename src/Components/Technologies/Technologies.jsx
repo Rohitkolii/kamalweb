e70d17f56dd6ab8c54a32402f8a759915d471292
Roohit koli <rohitkolisd@gmail.com>
2025-03-06 14:10:23 +0530

    fix 06-03-2025

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -42,8 +42,7 @@ const Technologies = ({sections}) => {
                     <div><img src="images/dynamodb.png"/></div>
                     <div><img src="images/aws.svg"/></div>
                     <div><img src="images/s3.png"/></div>
-                    <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Cloudflare_Logo.svg/800px-Cloudflare_Logo.svg.png"/></div>
-
+                    <div><img src="images/cloud.png"/></div>
                 </div>
     
     const generateTechBody = () => {
